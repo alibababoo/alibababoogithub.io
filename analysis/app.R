7337ec1d775402d3dd0e554bed44539b3266f825
alibababoo <E0968339@U.NUS.EDU>
2023-11-15 18:08:36 +0800

    yay

--- analysis/app.R
+++ analysis/app.R
@@ -1,7 +1,7 @@
-
 library(shiny)
 library(ggplot2)
 library(reshape2)
+library(plotly)
 
 # Reading the CSV file (replace "swiftdata.csv" with your file name)
 swiftdata <- read.csv("swiftdata.csv")
@@ -16,27 +16,32 @@ ui <- fluidPage(
                   selected = "acousticness")
     ),
     mainPanel(
-      plotOutput("boxplot")
+      plotlyOutput("boxplot")
     )
   )
 )
 
 # Server
 server <- function(input, output) {
-  output$boxplot <- renderPlot({
+  output$boxplot <- renderPlotly({
     # Subset data based on selected attribute
     selected_attr <- input$attribute
     melted_data <- melt(swiftdata[, selected_attr])
     
     # Create box plot
-    ggplot(melted_data, aes(x = 1, y = value)) +
+    p <- ggplot(melted_data, aes(x = 1, y = value)) +
       geom_boxplot() +
-      geom_jitter(color="pink", size=2, alpha=1) +
+      geom_jitter(color = "pink", size = 2, alpha = 1) +
       labs(x = "songs", y = selected_attr, title = paste("", selected_attr)) +
       scale_x_discrete(breaks = NULL) + # Removes x-axis values
       theme_minimal()
+    
+    # Convert ggplot to interactive plotly
+    ggplotly(p, tooltip = "y")
   })
 }
 
 # Run the application
 shinyApp(ui = ui, server = server)
+
+
